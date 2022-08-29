c4ad5098e5812e95d4022a8ef429eedf5ba1c5fc
fahminlb33 <fahminlb33@gmail.com>
2022-08-29 21:30:56 +0700

    Save app config (dark mode) to redis

--- RedisStatusPage/Data/GlobalAppState.cs
+++ RedisStatusPage/Data/GlobalAppState.cs
@@ -1,15 +1,24 @@
-﻿namespace RedisStatusPage.Data
+﻿using StackExchange.Redis;
+
+namespace RedisStatusPage.Data
 {
     public class GlobalAppState
     {
-        private bool _isDarkMode;
+        private const string DarkModeKey = "APP_CONFIG:" + nameof(IsDarkMode);
+
+        private readonly ConnectionMultiplexer _cn;
+
+        public GlobalAppState(ConnectionMultiplexer cn)
+        {
+            _cn = cn;
+        }
 
         public bool IsDarkMode
         {
-            get => _isDarkMode;
+            get => _cn.GetDatabase().StringGet(DarkModeKey) == "1";
             set
             {
-                _isDarkMode = value;
+                _cn.GetDatabase().StringSet(DarkModeKey, value);
                 NotifyStateChanged();
             }
         }
